41deadca3256de260aeadb183af8619c49497404
makshintre <makshintre@gmail.com>
2021-10-13 21:35:01 +0530

    Make Test fail4

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -16,7 +16,7 @@ void main() {
     await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0. test fail 10 mak
-    expect(find.text('10'), findsOneWidget);
+    expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
 
     // Tap the '+' icon and trigger a frame.
